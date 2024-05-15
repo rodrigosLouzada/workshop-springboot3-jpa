c25007deb2c9addb14c3eb7c91f4a28e48ccb2ae
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-05-15 11:04:59 -0300

    entity user, resource, incomplete

---
+++ src/main/java/com/courseSpring/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.courseSpring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.courseSpring.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User();
+		return ResponseEntity.ok().body(u);
+	}
+
+}
