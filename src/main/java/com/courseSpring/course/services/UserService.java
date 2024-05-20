666b1e569b4ab40f33416962d72b1300b2be7fec
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-05-20 17:53:57 -0300

    JPA repository, findAll, service, resource, incomplete

--- src/main/java/com/courseSpring/course/services/UserService.java
+++ src/main/java/com/courseSpring/course/services/UserService.java
@@ -3,10 +3,12 @@ package com.courseSpring.course.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.courseSpring.course.entities.User;
 import com.courseSpring.course.repositories.UserRepository;
 
+@Service
 public class UserService {
 	
 	@Autowired
