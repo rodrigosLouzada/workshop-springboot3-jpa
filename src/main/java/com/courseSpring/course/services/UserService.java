6f8a774daf74b78c0ca0bae8620a77718b35ceca
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-05-22 12:03:28 -0300

    service layer, component registration, findbyid, findall, complete

--- src/main/java/com/courseSpring/course/services/UserService.java
+++ src/main/java/com/courseSpring/course/services/UserService.java
@@ -1,6 +1,7 @@
 package com.courseSpring.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,4 +20,8 @@ public class UserService {
 		return repository.findAll();
 	}
 
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
 }
